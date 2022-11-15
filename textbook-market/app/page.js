65de244a1f22ca9bb28918c450e864636be989e1
Steven <steven.simko1000@gmail.com>
2022-11-14 19:11:53 -0600

    Added MongoDB

--- textbook-market/app/page.js
+++ textbook-market/app/page.js
@@ -1,4 +1,25 @@
 import React from "react";
+import clientPromise from "../lib/mongodb"
+
+const mongodb = async () => {
+  try {
+    await clientPromise
+
+    return {
+        props: {
+        isConnected: true
+      }
+    }
+  } catch(error) {
+    console.log(error)
+
+    return {
+      props: {
+        isConnected: false
+      }
+    }
+  }
+}
 
 export default function Home() {
   return (
