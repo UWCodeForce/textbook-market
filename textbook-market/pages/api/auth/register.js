4c9cb69ab15b2e88568282af1d63385b8e8adb80
Raghuboi <82239949+Raghuboi@users.noreply.github.com>
2022-11-30 16:12:47 +0530

    Improve /register route

--- textbook-market/pages/api/auth/register.js
+++ textbook-market/pages/api/auth/register.js
@@ -2,24 +2,23 @@ import dbConnect from '../../../lib/dbConnect'
 const User = require('../../../models/user')
 
 export default async function handler(req, res) {
-	const { username, password } = req.body
-	console.log(username, password)
+	if (req.method === 'POST') {
+		const { username, password } = req.body
 
-	try {
-		await dbConnect()
-		const exists = await User.findOne({ username: username })
-		console.log(exists)
+		try {
+			await dbConnect()
+			const exists = await User.findOne({ username: username })
 
-		if (exists) {
-			res.status(200).json({ message: 'Already Registered' })
+			if (exists) {
+				res.status(422).json({ message: 'Already Registered' })
+			} else {
+				const user = new User({ username, password })
+				await user.save()
+				res.status(201).json({ status: true, user: user })
+			}
+		} catch (e) {
+			console.error(e)
+			res.status(500).json({ error: e.message })
 		}
-
-		const user = new User({ username, password })
-		await user.save()
-		console.log('Success')
-		res.status(201).json({ message: 'Success' })
-	} catch (e) {
-		console.error(e)
-		res.status(500).json({ error: e.message })
-	}
+	} else res.status(500).json({ message: 'HTTP method not valid' })
 }
