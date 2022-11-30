4842aea0faaeb894b58e0f3bc087686638c1389c
Raghuboi <82239949+Raghuboi@users.noreply.github.com>
2022-11-30 14:16:59 +0530

    Create /register route

---
+++ textbook-market/pages/api/register.js
@@ -0,0 +1,25 @@
+import connectDB from '../../lib/connectDB'
+const User = require('../../models/user')
+
+export default async function handler(req, res) {
+	const { username, password } = req.body
+	console.log(username, password)
+
+	try {
+		connectDB()
+		const exists = await User.findOne({ username: username })
+		console.log(exists)
+
+		if (exists) {
+			res.status(200).json({ message: 'Already Registered' })
+		}
+
+		const user = new User({ username, password })
+		await user.save()
+		console.log('Success')
+		res.status(201).json({ message: 'Success' })
+	} catch (e) {
+		console.error(e)
+		res.status(500).json({ error: e.message })
+	}
+}
