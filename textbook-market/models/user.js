929630cad34d7876da9feab3d4fee5eff07e256a
Raghuboi <82239949+Raghuboi@users.noreply.github.com>
2022-12-01 06:23:00 +0530

    Run prettier

--- textbook-market/models/user.js
+++ textbook-market/models/user.js
@@ -1,36 +1,36 @@
-const mongoose = require('mongoose')
-const bcrypt = require('bcrypt')
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
-const SALT_WORK_FACTOR = 10
+const SALT_WORK_FACTOR = 10;
 
 var UserSchema = new mongoose.Schema({
-	username: { type: String, required: true, index: { unique: true } },
-	password: { type: String, required: true },
-})
+  username: { type: String, required: true, index: { unique: true } },
+  password: { type: String, required: true },
+});
 
-UserSchema.pre('save', function (next) {
-	var user = this
+UserSchema.pre("save", function (next) {
+  var user = this;
 
-	// only hash the password if it has been modified (or is new)
-	if (!user.isModified('password')) return next()
+  // only hash the password if it has been modified (or is new)
+  if (!user.isModified("password")) return next();
 
-	// generate a salt
-	bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
-		if (err) return next(err)
+  // generate a salt
+  bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
+    if (err) return next(err);
 
-		// hash the password using our new salt
-		bcrypt.hash(user.password, salt, function (err, hash) {
-			if (err) return next(err)
+    // hash the password using our new salt
+    bcrypt.hash(user.password, salt, function (err, hash) {
+      if (err) return next(err);
 
-			// override the cleartext password with the hashed one
-			user.password = hash
-			next()
-		})
-	})
-})
+      // override the cleartext password with the hashed one
+      user.password = hash;
+      next();
+    });
+  });
+});
 
 UserSchema.methods.validatePassword = async function validatePassword(data) {
-	return bcrypt.compare(data, this.password)
-}
+  return bcrypt.compare(data, this.password);
+};
 
-module.exports = mongoose.models.User || mongoose.model('User', UserSchema) // check if model already exists, if not create it
+module.exports = mongoose.models.User || mongoose.model("User", UserSchema); // check if model already exists, if not create it
