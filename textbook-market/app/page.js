79027584e1c0d9c05131f6eea15319ab26c1e59a
Raghunath Prabhakar <82239949+Raghuboi@users.noreply.github.com>
2022-11-30 18:58:19 -0600

    Set up basic authentication using credentials (#9)
    * Add client-only, server-only as dev-dependencies
    
    * Mark function as server-only
    
    * Revert "Mark function as server-only"
    
    This reverts commit 2fd7bda0c63ef768119a7851cfae68319ee2903d.
    
    * Update .gitignore
    
    * Remove redundant files
    
    * Add dependencies
    
    * Initialize mongoose
    
    * Create user model
    
    * Create /register route
    
    * Create sign-up page
    
    * Move /register route
    
    * Update dbConnect() as per Vercel docs
    
    https://github.com/vercel/next.js/blob/canary/examples/with-mongodb-mongoose/lib/dbConnect.js
    
    * Improve /register route
    
    * Initialize next-auth
    
    * Update user schema method
    
    * Create signin/signup pages
    
    * Delete package-lock.json
    
    * Run prettier
    
    * Run prettier

--- textbook-market/app/page.js
+++ textbook-market/app/page.js
@@ -1,28 +1,36 @@
 import React from "react";
-import clientPromise from "../lib/mongodb"
+import AuthButtons from "../components/AuthButtons";
+import clientPromise from "../lib/mongodb";
+import { unstable_getServerSession } from "next-auth/next";
 
 const mongodb = async () => {
   try {
-    await clientPromise
-
-    return {
-        props: {
-        isConnected: true
-      }
-    }
-  } catch(error) {
-    console.log(error)
+    await clientPromise;
 
     return {
       props: {
-        isConnected: false
-      }
-    }
-  }
-}
+        isConnected: true,
+      },
+    };
+  } catch (error) {
+    console.log(error);
 
-export default function Home() {
+    return {
+      props: {
+        isConnected: false,
+      },
+    };
+  }
+};
+
+export default async function Home() {
+  const session = await unstable_getServerSession();
+  const user = session?.user;
+
   return (
-    <h1>Welcome to our Textbook Market homepage</h1>
-  )
+    <>
+      <h1>Welcome {user?.name} to our Textbook Market homepage</h1>
+      <AuthButtons user={user} />
+    </>
+  );
 }
---
+++ textbook-market/pages/api/auth/register.js
@@ -0,0 +1,24 @@
+import dbConnect from "../../../lib/dbConnect";
+const User = require("../../../models/user");
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    const { username, password } = req.body;
+
+    try {
+      await dbConnect();
+      const exists = await User.findOne({ username: username });
+
+      if (exists) {
+        res.status(422).json({ message: "Already Registered" });
+      } else {
+        const user = new User({ username, password });
+        await user.save();
+        res.status(201).json({ status: true, user: user });
+      }
+    } catch (e) {
+      console.error(e);
+      res.status(500).json({ error: e.message });
+    }
+  } else res.status(500).json({ message: "HTTP method not valid" });
+}
